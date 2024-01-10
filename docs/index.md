3299557dcf595d6ddcf61ec8cabdaeef4d7e8db5
Lewis Fletcher <lew.fletcher3@gmail.com>
2024-01-09 20:20:50 -0700

    Preparing for rename.

---
+++ docs/index.md
@@ -0,0 +1,8 @@
+# Welcome to Django Help Me's documentation!
+
+Contents
+--------
+* [Installation](installation.md)
+* [Usage](usage.md)
+* [Settings](settings.md)
+* [Template tags](templatetags.md)
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-help-me',
+    version='1.0.0',
+    packages=find_packages(exclude=['*.migrations', '*.migrations.*', 'migrations.*', 'migrations', 'docs', 'docs.*']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=4.0',
+        'django-markdownx>=4.0.7',
+        'django-simple-history>=3.4.0',
+        'markdown>=3.5.1',
+        'django-htmx>=1.17.2'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
