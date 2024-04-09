299a48907536fc0156da07debc55552cfdc82a6e
Lewis Fletcher <lew.fletcher3@gmail.com>
2024-04-09 13:52:18 -0700

    Updating models to handle unique validation in regex in cases where the regex field is blank.

--- easy_docs/models.py
+++ easy_docs/models.py
@@ -12,7 +12,7 @@ class Documentation(models.Model):
     title = models.CharField(max_length=100, unique=True)
     title_slug = models.SlugField(max_length=100, unique=True)
     reference_url = models.CharField(max_length=200, unique=True, blank=True, null=True)
-    regex_url = models.CharField(max_length=200, unique=True, blank=True, null=True, help_text="Regular expression to match the URL of the page you are documenting, if any. Leave blank if this isn't needed.")
+    regex_url = models.CharField(max_length=200, blank=True, null=True, help_text="Regular expression to match the URL of the page you are documenting, if any. Leave blank if this isn't needed.")
     content = MarkdownxField()
     keywords = models.TextField(null=False, blank=True, help_text="Comma separated list of keywords to help users find this documentation.")
     public = models.BooleanField(default=False)
@@ -23,13 +23,14 @@ class Documentation(models.Model):
     history = HistoricalRecords()
 
     def save(self, *args, **kwargs):
+        self.clean()
         current_user = CurrentUserMiddleware.get_current_user()
         if current_user and not current_user.is_anonymous:
             self.updated_by = current_user
+            if not self.pk:
+                self.created_by = current_user
         if not self.title_slug:
             self.title_slug = slugify(self.title)[:100]
-            if current_user and not current_user.is_anonymous:
-                self.created_by = current_user
         super(Documentation, self).save(*args, **kwargs)
 
     def clean(self):
@@ -37,6 +38,14 @@ class Documentation(models.Model):
             existing = Documentation.objects.filter(reference_url=self.reference_url).exclude(pk=self.pk)
             if existing.exists():
                 raise ValidationError("A Documentation with this reference URL already exists.")
+        if self.regex_url:
+            existing = Documentation.objects.filter(regex_url=self.regex_url).exclude(pk=self.pk)
+            if existing.exists():
+                raise ValidationError("A Documentation with this regex URL already exists.")
+            try:
+                re.compile(self.regex_url)
+            except re.error:
+                raise ValidationError("Invalid regular expression.")
         super(Documentation, self).clean()
 
     def __str__(self):
